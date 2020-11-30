97c0d5e4992873e6790d6db62ba54f2ad1f195d4
PensActius <37923848+pensactius@users.noreply.github.com>
2020-11-30 11:13:16 +0100

    Add files via upload

---
+++ Cooche4x4/Arduino/CocheTestMotores/CocheTestMotores.ino
@@ -0,0 +1,21 @@
+int rightA = 10;
+int rightB = 9;
+int leftA = 5;
+int leftB = 6;
+int vel = 255;
+void setup() {
+pinMode (rightA, OUTPUT);
+pinMode (rightB, OUTPUT);
+pinMode (leftA, OUTPUT);
+pinMode (leftB, OUTPUT);
+
+}
+
+void loop() {
+analogWrite (rightA, 255);
+analogWrite (rightB, 0);
+analogWrite (leftA, 255);
+analogWrite (leftB, 0);
+
+
+}
